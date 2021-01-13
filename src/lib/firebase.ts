807f8360a845015adbf5b9f540121de7e497a45c
hector vinicio lopez molinares <hectorvmlopez@gmail.com>
2021-01-13 09:36:41 -0500

    first commit, ussing less preprocessor, boostrap

---
+++ src/lib/firebase.ts
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+export const firebaseConfig = {
+    apiKey: "AIzaSyBvL9g-o8b_-vOwBsdB3o5WAqV9Wj16t_A",
+    authDomain: "ui-clone-142e1.firebaseapp.com",
+    projectId: "ui-clone-142e1",
+    storageBucket: "ui-clone-142e1.appspot.com",
+    messagingSenderId: "383824309656",
+    appId: "1:383824309656:web:acd999f6182bc28eb84fed"
+  };
+
+firebase.initializeApp(firebaseConfig);
